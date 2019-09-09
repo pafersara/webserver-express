2e27e464020b29f741cdbfbca4fd585bddfe34f7
Fernando <pafersara@gmail.com>
2019-09-09 16:02:12 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+
+app.set('view engine', 'hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Fernando'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+});
+
+app.get('/data', (req, res) => {
+
+    res.send('Hola data');
+
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
